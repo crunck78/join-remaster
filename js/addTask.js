ebe0d1b3f2bde80bc2d0747a2ff3a5437fd4576a
crunck78 <crunck78@googlemai.com>
2020-12-18 20:40:18 +0100

    add task css

--- js/addTask.js
+++ js/addTask.js
@@ -10,3 +10,30 @@ window.onload = function () {
     }
   });
 };
+
+function createTask(){
+	let newTask = {
+		title: document.getElementById('title').value,
+		category : document.getElementById('category').value,
+		description : document.getElementById('description').value,
+		dueDate : document.getElementById('dueDate').value,
+		importance : document.getElementById('importance').value,
+		assignee : []
+	};
+}
+
+async function openDialog(){
+	await getUsers();
+	document.getElementById('dialog-container').classList.remove('d-none');
+}
+
+function getUsers(){
+	return joinDatabase.ref('/users')
+	.once('value')
+	.then( (snapshot) =>{
+		const allUsers = snapshot.val();
+	})
+	.catch( (error) =>{
+		console.error( error );
+	});
+}
